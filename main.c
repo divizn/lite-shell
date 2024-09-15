3d4c1df0297fe6467da046cfc7e98cc947c529e6
phon <hassanjaved186@gmail.com>
2024-09-15 08:29:44 +0100

    init

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char input[100];
+
+  while (1) {
+    printf("$ ");
+
+    fflush(stdout);
+    fgets(input, 100, stdin);
+    input[strlen(input) - 1] = '\0';
+
+    printf("%s: command not found\n", input);
+  }
+
+  return 0;
+}
