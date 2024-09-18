7b915674b7d071136e769fe04d7284df3e41422d
phon <hassanjaved186@gmail.com>
2024-09-18 03:04:40 +0100

    feat: add exit command

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+const char *COMMANDS[1] = {"exit"};
+
+void execute_command(char *command);
 
 int main() {
   char input[100];
@@ -10,9 +16,25 @@ int main() {
     fflush(stdout);
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
-
-    printf("%s: command not found\n", input);
+    
+    for (int i=0; i < sizeof(COMMANDS)/sizeof(COMMANDS[0]); i++) {
+      if (input == "") {
+        break;
+      }
+      if (strcmp(input, COMMANDS[i]) == 0) {
+        execute_command(input);
+      }
+      else {
+        printf("scsh: command not found: %s\n", input);
+      }
+    }
   }
 
   return 0;
+}
+
+void execute_command(char *command) {
+  if (strcmp(command, "exit") == 0) {
+    exit(0);
+  }
 }
